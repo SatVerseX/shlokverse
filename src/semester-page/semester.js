9f9fc0c02459970feccbd5b3e6dcfbb3d5a869b3
satish pal <satish@151104.com>
2024-08-01 16:53:42 +0530

    update

--- src/semester-page/semester.js
+++ src/semester-page/semester.js
@@ -4,13 +4,10 @@ import React from 'react';
 import './semester.css'; // Import the CSS file
 
 const FileDownload = () => {
-
   const files = [
-    { name: 'Semester 1 - English', url: './English class notes (unit 1, 2, 3).pdf' },
-    { name: 'Semester 2 - Physics', url: 'files/sem2-physics.pdf' },
-    { name: 'Semester 3 - Chemistry', url: 'files/sem3-chemistry.pdf' },
-
-  
+    { name: 'Semester 1 - English', url: 'https://raw.githubusercontent.com/SatVerseX/shlokverse/main/docs/English%20class%20notes%20%28unit%201%2C%202%2C%203%29.pdf' },
+    { name: 'Semester 2 - Physics', url: 'https://raw.githubusercontent.com/SatVerseX/shlokverse/main/docs/sem2-physics.pdf' }, // Example URL
+    { name: 'Semester 3 - Chemistry', url: 'https://raw.githubusercontent.com/SatVerseX/shlokverse/main/docs/sem3-chemistry.pdf' }, // Example URL
   ];
 
   const handleDownload = (fileUrl, fileName) => {
