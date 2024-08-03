7961e2471338f7e5db4cca8f169ccd1a5d12a171
SatVerseX <satish151104@gmail.com>
2024-08-03 09:54:42 +0530

    Update carousel component and remove deleted images

--- src/page1-components/Carousel.js
+++ src/page1-components/Carousel.js
@@ -1,36 +1,35 @@
 import React, { useState } from 'react';
 import { useSwipeable } from 'react-swipeable';
 import './carousel.css'; // Ensure the path is correct
-import Image1 from '../image/_107f2cb6-894d-44e2-a691-797aa4d5f98f.jpeg';
-import Image2 from '../image/_431c29bf-6070-4a14-be41-d40b3b4dc8dd.jpeg';
-import Image3 from '../image/_9f50b374-7e3e-4148-89df-02b68a12e29f.jpeg';
-import Image4 from '../image/_bc9451b9-0dc1-44f9-b28a-c4949214f762.jpeg';
-import Image5 from '../image/_c2720809-35c3-4ae9-a74a-c80c109480a9.jpeg';
+import Image1 from '../image/R (1).png';
+import Image2 from '../image/R (2).png';
+import Image3 from '../image/R (3).png';
+import Image4 from '../image/pngtree-juicy-burgers-with-a-transparent-background-png-image_9002761.png';
+import Image5 from '../image/pngtree-shree-ganeshay-namah-hindi-calligraphy-with-lord-ganesha-symbol-design-png-image_8987150.png';
 
 const images = [Image1, Image2, Image3, Image4, Image5];
 
 const Carousel = () => {
-  const [currentIndex, setCurrentIndex] = useState(2); // Start with the middle image
+  const [currentIndex, setCurrentIndex] = useState(0); // Start with the first image
   const [isHovered, setIsHovered] = useState(false); // To handle button visibility on hover
 
   const handlers = useSwipeable({
-    onSwipedLeft: () => nextImage(),
-    onSwipedRight: () => prevImage(),
+    onSwipedLeft: () => prevImage(),
+    onSwipedRight: () => nextImage(),
     preventDefaultTouchmoveEvent: true,
     trackMouse: true
   });
 
   const nextImage = () => {
-    setCurrentIndex((currentIndex + 1) % images.length);
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
   const prevImage = () => {
-    setCurrentIndex((currentIndex - 1 + images.length) % images.length);
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
   };
 
   const getHighlightClass = (index) => {
-    const middleIndex = (currentIndex + 1) % images.length;
-    return index === middleIndex ? 'active' : '';
+    return index === currentIndex ? 'active' : '';
   };
 
   return (
